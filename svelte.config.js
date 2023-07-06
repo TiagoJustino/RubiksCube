6c7b0cd160f1e4083345c17bf12bfd894095b814
Tiago Justino <tiago.vmj@gmail.com>
2023-07-05 22:01:53 -0300

    added github actions for github deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,14 @@
-import { vitePreprocess } from 'npm:@sveltejs/vite-plugin-svelte@^2.4.1'
+import { vitePreprocess } from "npm:@sveltejs/vite-plugin-svelte@^2.4.1";
+import adapter from "npm:@sveltejs/adapter-static@^2.0.2";
+
+const dev = Deno.args.includes("dev");
 
 export default {
-  preprocess: vitePreprocess()
-}
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: dev ? "" : process.env.BASE_PATH,
+    },
+  },
+};
